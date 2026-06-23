3ac5d0aac9399dd195a313fe9f0d7811fee8e4d1
nitish-dass <dass2005nd@gmail.com>
2026-06-23 15:06:34 +0545

    created register page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from './pages/Landing'
 import About from './pages/About'
+import Register from './pages/Register'
 
 const App = () => {
   return (
@@ -9,6 +10,9 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/about" element={<About />} />
+
+      
+      <Route path="/register" element={<Register />} />
     </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Register.jsx
@@ -0,0 +1,172 @@
+import * as z from "zod";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Field, FieldError, FieldLabel } from "../components/ui/field";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import { Eye, EyeOff } from "lucide-react";
+
+const formSchema = z
+  .object({
+    name: z.string().min(3, "Name should be at least 3 characters. "),
+    email: z.string().email("Invalid email address."),
+    password: z.string().min(8, "Password should be at least 8 characters."),
+    confirmPassword: z
+      .string()
+      .min(8, "Password should be at least 8 characters,"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password does not match.",
+    path: ["confirmPassword"],
+  });
+
+const Register = () => {
+  
+  const [show, setShow] = React.useState(false);
+  const [showConfirm, setShowConfirm] = React.useState(false);
+  
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={form.handleSubmit(onSubmit)}>
+      <Card className="w-1/4 mx-auto mt-32">
+        <CardHeader>
+          <CardTitle>Register to WanderWise</CardTitle>
+          <CardDescription>
+            Fill your details to create an account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <Controller
+            name="name"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor={field.name}>Full Name</FieldLabel>
+                <Input
+                  {...field}
+                  id={field.name}
+                  type="text"
+                  placeholder="John Doe"
+                  aria-invalid={fieldState.invalid}
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+          <Controller
+            name="email"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                <Input
+                  {...field}
+                  id={field.name}
+                  type="email"
+                  placeholder="abc@gmail.com"
+                  aria-invalid={fieldState.invalid}
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+
+          <div className="flex items-end gap-1 ">
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                  <Input
+                    {...field}
+                    id={field.name}
+                    type={show ? "text" : "password"}
+                    placeholder="********"
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            
+            <Button onClick={() => {setShow(!show)}} type="button" size="icon" variant="outline">
+              {
+                show ? < EyeOff /> : <Eye />
+              }
+            </Button>
+          </div>
+
+          <div className="relative ">
+          <Controller
+            name="confirmPassword"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor={field.name}>Confirm Password</FieldLabel>
+                <Input
+                  {...field}
+                  id={field.name}
+                  type={showConfirm ? "text" : "password"}
+                  placeholder="********"
+                  aria-invalid={fieldState.invalid}
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+          
+          <div className="absolute right-1 bottom-1.5 " onClick={() => {setShowConfirm(!showConfirm)}} >
+            {
+              showConfirm ? <EyeOff size={18} /> : <Eye size={18} />
+            }
+          </div>
+
+          </div>
+        </CardContent>
+        <CardFooter className="block">
+          <Button types="submit" className="w-full">Register</Button>
+
+          <div className="mt-2 text-center">
+            Already have an account?
+            <a className="text-blue-500 underline" href="/login">LogIn</a>
+          </div>
+
+        </CardFooter>
+      </Card>
+    </form>
+  );
+};
+
+export default Register;
